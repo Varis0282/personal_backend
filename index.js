9f4af8a4a9c9cd93d175989310a52ec1a0365ccb
Varis0282 <varisrajakforth11@gmail.com>
2024-02-12 21:47:24 +0530

    Serverless Running enabled 2

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const app = express();
 require('dotenv').config();
-const db = require('./dbconfig');
 const authRoutes = require('./routes/auth_route');
 const userRoutes = require('./routes/user_routes');
 const adminRoutes = require('./routes/admin_routes');
 const cors = require('cors');
+const mongoose = require('mongoose');
+const Mongo_URI = process.env.Mongo_URI;
+const port = process.env.PORT;
 
 app.use(express.json());
 app.use(cors());
@@ -13,7 +15,26 @@ app.use(cors());
 app.use('/api/auth', authRoutes);
 app.use('/api/admin', adminRoutes);
 app.use('/api/me', userRoutes);
+
+app.use('*', (req, res) => {
+    res.status(404).json({ message: 'Api not found' });
+});
+
 app.get('/', (req, res) => {
     res.send('Hello World');
 });
 
+mongoose.connect(Mongo_URI)
+    .then(() => {
+        console.log('Connected to the database');
+        try {
+            app.listen(port, () => {
+                console.log(`Server is running on ${port}`);
+            });
+        } catch (err) {
+            console.log('Error starting the server',err);
+        }
+    })
+    .catch((err) => {
+        console.log('Error connecting to the database');
+    });
