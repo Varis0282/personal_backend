797697929d4f08391ae2e15f7644473bdecc14b1
Varis0282 <varisrajakforth11@gmail.com>
2024-02-13 12:59:31 +0530

    user return in verify otp

--- routes/auth_route.js
+++ routes/auth_route.js
@@ -139,7 +139,7 @@ router.post('/verify-email', async (req, res) => {
         user.mailVerified = true;
         user.lastOTP = '';
         await user.save();
-        res.status(200).json({ message: 'Email verified successfully', success: true });
+        res.status(200).json({ message: 'Email verified successfully', success: true, user: user });
     } catch (error) {
         res.status(500).json({ message: error.message, success: false });
     }
